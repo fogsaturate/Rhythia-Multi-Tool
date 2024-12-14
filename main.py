a55b0c0459790a6afc050b497ea193fd39577b7e
fogsaturate <brangaming95@gmail.com>
2024-12-13 23:54:38 -0500

    main stuff

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import parsers.rhym
+import parsers.vulnus
 import os
 
-Parser = parsers.rhym.RhymParser()
+RhymParser = parsers.rhym.RhymParser()
+VulnusParser = parsers.vulnus.VulnusParser()
+
 main_path = os.path.dirname(__file__)
+
 rhym_path = os.path.join(main_path, "Test Maps", "rhym")
 rhym_map_path = os.path.join(rhym_path, "Haxagon_iyowa_Kyu-kurarin_(7_7_bootleg)")
+
+vulnus_path = os.path.join(main_path, "Test Maps", "vulnus")
+vulnus_map_path = os.path.join(vulnus_path, "Multipole Expansion")
 # lmfao
 
 
-rhym_test_map = Parser.RhymDecoder(rhym_map_path)
-print(rhym_test_map.difficulty_metadata)
+rhym_test_map = RhymParser.RhymDecoder(rhym_map_path)
+vulnus_test_map = VulnusParser.VulnusDecoder(vulnus_map_path)
+
+print(vulnus_test_map.difficulty_data)
