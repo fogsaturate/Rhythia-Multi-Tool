4badd1e648ae391a4dc7f6b6342b1eeb5f3cc501
fogsaturate <brangaming95@gmail.com>
2024-12-14 14:33:54 -0500

    encoder showcase

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ VulnusParser = parsers.vulnus.VulnusParser()
 
 main_path = os.path.dirname(__file__)
 
+
+
 rhym_path = os.path.join(main_path, "Test Maps", "rhym")
 rhym_map_path = os.path.join(rhym_path, "Haxagon_iyowa_Kyu-kurarin_(7_7_bootleg)")
+rhym_output_path = os.path.join(main_path, "Output", "rhym")
 
 vulnus_path = os.path.join(main_path, "Test Maps", "vulnus")
 vulnus_map_path = os.path.join(vulnus_path, "Multipole Expansion")
@@ -17,29 +20,38 @@ vulnus_map_path = os.path.join(vulnus_path, "Multipole Expansion")
 
 rhym_map = RhymParser.RhymDecoder(rhym_map_path)
 vulnus_map = VulnusParser.VulnusDecoder(vulnus_map_path)
+
+vulnus_map_music_path = os.path.join(vulnus_map_path, vulnus_map.audio_filename)
+vulnus_map_cover_path = os.path.join(vulnus_map_path, "cover.png")
+
+
+# testing only
+
+vulnus_map_difficulty_data = []
+current_difficulty_name = []
+
+for i in vulnus_map.difficulty_data:
+    vulnus_difficulty_data = {
+        "difficulty_name": i["difficulty_name"],
+        "mappers": vulnus_map.mappers, # theres not really any other way to do this unfortunately
+        "note_count": i["note_count"],
+        "note_fields": 3,
+        "note_list": i["note_list"] # in x, y, time (in ms) format. example: {'x': 0, 'y': 0, 'time': 100}
+    }
+    vulnus_map_difficulty_data.append(vulnus_difficulty_data)
+
+print(vulnus_map_music_path)
+print(vulnus_map_cover_path)
+
 RhymParser.RhymEncoder(
     vulnus_map.version,
     vulnus_map.artist,
     vulnus_map.title,
     vulnus_map.difficulties,
-    [
-        {
-          'difficulty_name': 'example',
-          'artist': 'exampleartist',
-          'romanized_artist': 'optionalartist',
-          'title': 'exampletitle',
-          'romanized_title': 'optionaltitle',
-          'mappers': ['example mapper 1'],
-          'note_count': 100
-        }
-    ],
-    [
-        {
-            'note_fields': 3,
-            'note_list': [{'x': 0, 'y': 1, 'time': 100},{'x': 1, 'y': -1, 'time': 200}]
-        }
-    ],
-    romanized_artist="Bob"
+    vulnus_map_difficulty_data,
+
+    rhym_output_path,
+    music_path=vulnus_map_music_path,
+    cover_path=vulnus_map_cover_path
 )
 
-# print(rhym_map.difficulty_objectdata)
